00550c3ea1dd75219fb263aef5b0ba2a390c61be
WendelHime <wendelhime@hotmail.com>
2018-02-09 13:20:20 -0200

    update password size

--- User.py
+++ User.py
@@ -1,6 +1,4 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.orm import relationship
-import database.Type
+from sqlalchemy import Column, String, Integer
 from models.User import User as UserModel
 from repository.base import Base
 
@@ -11,14 +9,12 @@ class User(Base.Base):
     id = Column(Integer, primary_key=True)
     email = Column(String(200))
     username = Column(String(20))
-    password = Column(String(256))
+    password = Column(String(2000))
     salt = Column(String(32))
     dateInsertion = Column('date_insertion', String(100))
     dateUpdate = Column('date_last_update', String(100))
-    #idType = Column('id_type', Integer, ForeignKey('types.id'))
     idType = Column('id_type', Integer)
-    #type = relationship('Type', back_populates='users')
-    
+
     def __init__(self,
                  id=0,
                  email="",
@@ -28,7 +24,6 @@ class User(Base.Base):
                  dateInsertion="",
                  dateUpdate="",
                  idType=0,
-                 #type=object(),  # database.Type.Type(),
                  user=UserModel):
         if (user.id or user.username):
             self.id = user.id
@@ -39,7 +34,6 @@ class User(Base.Base):
             self.dateInsertion = user.dateInsertion
             self.dateUpdate = user.dateUpdate
             self.idType = user.idType
-            #self.type = database.Type.Type(user.type)
         else:
             self.id = id
             self.email = email
@@ -49,4 +43,3 @@ class User(Base.Base):
             self.dateInsertion = dateInsertion
             self.dateUpdate = dateUpdate
             self.idType = idType
-            #self.type = type
