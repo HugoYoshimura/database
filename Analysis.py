3de243cd120b251ef14ee46da5e4b872756ee81e
HugoYoshimura <hugomasayoshi@gmail.com>
2018-03-24 15:18:20 -0300

    Fixed Analysis Database

--- Analysis.py
+++ Analysis.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 import database.Image
+import database.Classifier
 from models.Analysis import Analysis as AnalysisModel
 from repository.base import Base
 
@@ -9,14 +10,23 @@ class Analysis(Base.Base):
 
     id = Column(Integer, primary_key=True)
     idImage = Column('id_image', Integer, ForeignKey('images.id'))
+    idClassifier = Column('id_classifier', Integer, ForeignKey('classifiers.id'))
+    image = relationship('Image', back_populates='analysis')
+    classifier = relationship('Classifier', back_populates='analysis')
+
 
     def __init__(self,
                  id=0,
                  idImage=0,
+		 image=object(),
+		 idClassifier=0,
+		 classifier=object(),
                  analysis=AnalysisModel()):
-        if (analysis.id or analysis.idImage):
+        if (analysis.id or analysis.image.id or analysis.classifier.id):
             self.id = analysis.id
-            self.idImage = analysis.idImage
+            self.idImage = analysis.image.id
+	    self.idClassifier = analysis.classifier.id
         else:
             self.id = id
             self.idImage = idImage
+	    self.idClassifier = idClassifier
