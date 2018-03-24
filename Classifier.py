a47a78f8c651aee6f71d4db48dcad20f8836503b
HugoYoshimura <hugomasayoshi@gmail.com>
2018-03-23 21:40:50 -0300

    Update Classifier Database

--- Classifier.py
+++ Classifier.py
@@ -1,8 +1,12 @@
+
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 import database.Plant
+import database.Analysis
 from models.Classifier import Classifier as ClassifierModel
 from repository.base import Base
+
+
 
 class ClassifierDB(Base.Base):
 	__tablename__ = 'classifiers'
@@ -11,15 +15,21 @@ class ClassifierDB(Base.Base):
 	idPlant = Column('id_plant', Integer, ForeignKey('plants.id')
 	tag = Column('tag', String(2000))
 	path = Column('path', String(2000))
+	plant = relationship('Plant', back_populates='classifiers')
+	analyses = relationship('Analysis', lazy='subquery', back_populates='classifiers')
 
-	def __init__(self, id=0, idPlant=0, tag="", path="", classifier=ClassifierModel()):
+	def __init__(self, id=0, idPlant=0, tag="", path="", plant=object(), analyses=[], classifier=ClassifierModel()):
 		if (classifier.id):
 			self.id = classifier.id
-			self.idPlant = classifier.idPlant
+			self.idPlant = classifier.plant.id
 			self.tag = classifier.tag
 			self.path = classifier.path
+			self.analyses = []
+			for analysis in classifier.analyses:
+				self.analyses.append(database.Analysis.Analysis(analysis))
 		else:
 			self.id = id
-                        self.idPlant = idPlant
+                        self.idPlant = classifier.plant.id
                         self.tag = tag
                         self.path = path
+			self.analyses = analyses
